c35f938f9435d4e51bcbbe99c6625114408f3d5a
Łukasz Wojciechowski <nostrzak@gmail.com>
2019-01-21 11:49:59 +0100

    Improve build script to provide UMD

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,74 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from "rollup-plugin-commonjs";
+import replace from "rollup-plugin-replace";
+import { uglify } from "rollup-plugin-uglify";
 
-const config = {
+const deps = Object.keys(pkg.dependencies || {})
+const peers = Object.keys(pkg.peerDependencies || {})
+const allExternal = [...deps, ...peers]
+
+const makeExternalPredicate = externalsArr => {
+  if (externalsArr.length === 0) {
+      return () => false
+  }
+  const externalPattern = new RegExp(`^(${externalsArr.join('|')})($|/)`)
+  return id => externalPattern.test(id)
+}
+
+const babelConfig = {
+  babelrc: false,
+  configFile: false,
+  runtimeHelpers: true,
+  exclude: /node_modules\/(!callbag-)/,
+  ...require('./babel.config.js')
+}
+
+const createConfig = ({ output, min = false, external = 'all', env }) => ({
   input: './src/index.js',
-  output: [
-    {
-      format: 'es',
-      file: pkg.module
-    },
-    {
-      format: 'cjs',
-      file: pkg.main
-    }
-  ],
+  output,
+  external: makeExternalPredicate(external === 'all' ? allExternal : peers),
   plugins: [
     resolve(),
     commonjs({ include: 'node_modules/**' }),
     babel({
-      exclude: 'node_modules/**'
+      ...babelConfig,
+      plugins: [
+          ['@babel/plugin-transform-runtime',
+          { useESModules: output.format !== 'cjs' }
+        ]
+      ].concat(babelConfig.plugins || [])
     }),
+    env &&
+      replace({
+          'process.env.NODE_ENV': JSON.stringify(env),
+      }),
+    min && uglify()
   ]
-};
+});
 
-export default config;
+
+export default [
+  createConfig({
+    output: {
+      format: 'es',
+      file: pkg.module
+    }
+  }),
+  createConfig({
+    output: {
+      format: 'cjs',
+      file: pkg.main
+    }
+  }),
+  createConfig({
+    output: {
+      format: 'umd',
+      file: pkg.unpkg,
+      name: 'LiveChat'
+    },
+    external: 'peers',
+    env: 'production',
+    min: true,
+  }),
+];
